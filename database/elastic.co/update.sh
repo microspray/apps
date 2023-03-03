2a9766b8a9e3ab6648cb2f4b8efef9b546feb406
ant31 <2t.antoine@gmail.com>
2023-03-03 16:25:55 +0100

    Elasticsearch: update 2.6

--- database/elastic.co/update.sh
+++ database/elastic.co/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.5.0
+VERSION=2.6.0
 rm -rf manifests/upstream
 mkdir -p manifests/upstream
 curl -L https://download.elastic.co/downloads/eck/$VERSION/crds.yaml > manifests/upstream/crds.yaml
