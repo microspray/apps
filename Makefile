2ce1cbb8e4d94edf9f8a5b9cedd3230a25943be6
ant31 <2t.antoine@gmail.com>
2023-03-03 16:47:20 +0100

    Generate output

--- Makefile
+++ Makefile
@@ -2,3 +2,12 @@
 clean:
 	find . -name '*~' -exec rm -f {} +
 	find . -name '.#*' -exec rm -f {} +
+all: lint
+
+.PHONY: gen-all lint
+gen-all:
+	CURRENT_ENV=$(CURRENT_ENV) ./deploy-utils.sh gen-all
+lint:
+	CURRENT_ENV=$(CURRENT_ENV) ./deploy-utils.sh lint
+
+check: gen-all lint
---
+++ deploy-utils.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+makefile=`pwd`/apps.Makefile
+current=`pwd`
+ENVS=${CURRENT_ENV:-"."}
+# git crypt unlock /etc/git-crypt/conny-infra.key
+for envpath in $ENVS; do
+    echo $envpath
+    cd $current/$envpath
+    for f in $(find . -name kustomization.yaml -not -path "*/gen-output/*"); do
+        dir=$current/$envpath/"$(dirname "$f")"
+        cd $dir
+
+        if [ $1 = 'gen-all' ]; then
+            echo "GEN: $dir"
+            cp "$makefile" "$dir"
+            mv $dir/apps.Makefile $dir/apps.Makefile
+            make gen -f $dir/apps.Makefile
+           if [ $? -ne 0 ]; then
+               echo "Error with: $dir"
+               exit 1
+           fi
+        elif [ $1 = 'lint' ]; then
+            echo "LINT: $dir"
+            if ! kubeconform --ignore-missing-schemas --summary --strict $dir/gen-output ; then
+                echo "failed to validate $f"
+                exit 1
+            fi
+        else
+            echo "wrong command: chose lint or gen-all"
+        fi
+    done
+done
+exit 0
