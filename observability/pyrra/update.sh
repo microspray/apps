740aef70fd3fdbb427a7ca323b7efc6c3f9b2a62
Lucas Servén Marín <lserven@gmail.com>
2023-10-10 14:26:00 +0200

    pyrra: update and add webhook configuration
    This commit marks a big update in the configuration for Pyrra. The
    project recently switched from Cue to Jsonnet, which has changed the
    shape of the files and thus how we download them. Most importantly, this
    commit updates Pyrra to 0.7.0 and adds configuration for a validating
    webhook.
    
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>
    
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- observability/pyrra/update.sh
+++ observability/pyrra/update.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
+rm -rf manifests/upstream
 mkdir -p manifests/upstream
-(cd manifests/upstream && curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/config/crd/bases/pyrra.dev_servicelevelobjectives.yaml)
-(cd manifests/upstream && curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/config/rbac/role.yaml)
-(cd manifests/upstream && curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/config/api.yaml)
-(cd manifests/upstream && curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/config/kubernetes.yaml)
+(cd manifests/upstream && curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/examples/kubernetes/manifests-webhook/setup/pyrra-slo-CustomResourceDefinition.yaml)
+(cd manifests/upstream && printf "apiDeployment\napiService\napiServiceAccount\napiServiceMonitor\ncertificate\nissuer\nkubernetesClusterRole\nkubernetesClusterRoleBinding\nkubernetesDeployment\nkubernetesService\nkubernetesServiceAccount\nkubernetesServiceMonitor\nwebhook" | xargs -I {} curl -fsSO https://raw.githubusercontent.com/pyrra-dev/pyrra/main/examples/kubernetes/manifests-webhook/pyrra-{}.yaml)
