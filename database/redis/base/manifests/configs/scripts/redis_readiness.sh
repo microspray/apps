f010fa197457ccf4ca39713e69b52d84baee46ee
ant31 <2t.antoine@gmail.com>
2024-01-29 12:45:57 +0100

    Generate default redis password

--- database/redis/base/manifests/configs/scripts/redis_readiness.sh
+++ database/redis/base/manifests/configs/scripts/redis_readiness.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+REDIS_USER=${REDIS_USER:-"default"}
+REDIS_PASSWORD=${REDIS_PASSWORD:-"default"}
+REDIS_PORT=${REDIS_PORT:-"6379"}
+REDIS_HOST=${REDIS_HOST:-"0.0.0.0"}
+uri="redis://$REDIS_USER:$REDIS_PASSWORD@$REDIS_HOST:$REDIS_PORT"
+
 response=$(
   redis-cli \
-    -h localhost \
-    -p 6379 \
+    -u $uri \
     ping
 )
 if [ "$response" != "PONG" ] ; then
--- database/redis/base/manifests/configs/scripts/sentinel_liveness.sh
+++ database/redis/base/manifests/configs/scripts/sentinel_liveness.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+REDIS_SENTINEL_HOST=${REDIS_SENTINEL_HOST:-"0.0.0.0"}
+REDIS_SENTINEL_PORT=${REDIS_SENTINEL_PORT:-"26379"}
+REDIS_USER=${REDIS_USER:-"default"}
+REDIS_PASSWORD=${REDIS_PASSWORD:-"default"}
+uri="redis://$REDIS_USER:$REDIS_PASSWORD@$REDIS_SENTINEL_HOST:$REDIS_SENTINEL_PORT"
+
 response=$(
   redis-cli \
-    -h localhost \
-    -p 26379 \
+    -u $uri \
     ping
 )
 if [ "$response" != "PONG" ]; then
