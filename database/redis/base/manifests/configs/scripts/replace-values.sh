06577cd31e3edb784bfeb0b88f73cfc2341f2f30
ant31 <2t.antoine@gmail.com>
2024-01-31 14:09:35 +0100

    Redis: increase default down-after, and parameterize the value

--- database/redis/base/manifests/configs/scripts/replace-values.sh
+++ database/redis/base/manifests/configs/scripts/replace-values.sh
@@ -3,6 +3,7 @@ REDIS_NAME=${REDIS_NAME:-"myMaster"}
 REDIS_MASTER=${REDIS_INITIAL_MASTER_HOST:-"localost"}
 REDIS_PASSWORD=${REDIS_PASSWORD:-"default"}
 QUORUM=${QUORUM:-"2"}
+DOWN_AFTER=${DOWN_AFTER:-"5000"}
 POD_FQDN=${POD_FQDN:-"localhost"}
 sed_values() {
     file=$1
@@ -11,6 +12,7 @@ sed_values() {
     sed -i "s/__REDIS_PASSWORD__/${REDIS_PASSWORD}/g" $file
     sed -i "s/__POD_FQDN__/${POD_FQDN}/g" $file
     sed -i "s/__QUORUM__/${QUORUM}/g" $file
+    sed -i "s/__DOWN_AFTER__/${DOWN_AFTER}/g" $file
     sed -i "s/__HEADLESS_SVC__/${HEADLESS_SVC}/g" $file
     sed -r -i "s/(sentinel monitor .*)[0-9]$/\1 ${QUORUM}/g" $file
     if [[ "$POD_FQDN" == "$REDIS_MASTER" ]] ; then
